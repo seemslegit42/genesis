13e819b63db0f938e8496e9f7da7e11c24a438ed
Bryan Marlow <brylow@aevon.cloud>
2025-07-12 04:07:13 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -13,13 +13,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAy-rGaNnGg79pvWaHxDUpz7_ZAw-jXux8",
+  authDomain: "von-os-juooq.firebaseapp.com",
+  projectId: "von-os-juooq",
+  storageBucket: "von-os-juooq.appspot.com",
+  messagingSenderId: "366247376303",
+  appId: "1:366247376303:web:8ea58a9e3e93e9f981eb5a",
+  measurementId: "G-4KHLM1KLRB"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
